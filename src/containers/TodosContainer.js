e5e271845d0921e8e67b550ffcd52bb536e8ffa3
Brock Whitbread <brock.whitbread@gmail.com>
2019-03-01 12:23:37 -0800

    adds a container for Todos, a TodosList component and a Todo component

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import TodoModel from '../models/Todo';
+import TodosList from '../components/TodosList';
 
 class TodosContainer extends React.Component {
+  state = {
+    todos: []
+  };
+
+  // When TodosContainer mounts, fetch todo data
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  // Fetch all todos from super-crud-api
+  fetchData() {
+    TodoModel.all().then( (res) => {
+      this.setState({
+        todos: res.data.todos,
+        todo: ''
+      })
+    });
+  }
+
   render() {
     return (
-      <h2>I am the TodosContainer</h2>
+      <div className="todosContainer">
+        <TodosList todos={ this.state.todos } />
+      </div>
     )
   }
 }
